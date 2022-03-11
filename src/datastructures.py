5edcffaf462d1901af0e3a8430cfa2a62f626c7d
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-11 19:28:46 +0000

    Implement datastructures specification

--- src/datastructures.py
+++ src/datastructures.py
@@ -26,17 +26,23 @@ class FamilyStructure:
 
         return randint(0, 99999999)
 
+    # Assumes member has name (Str), age (Int), lucky_numbers ([Int])
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        self._members.append({
+            "id": self._generateId(),
+            "last_name": self.last_name,
+            **member,
+        })
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        self._members = [member for member in self._members if member.id != id]
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        return next[
+            # We find the member with the given id
+            [member for member in self._members if member.id == id],
+            # We set None as default to return it if no member is found
+            None]
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
