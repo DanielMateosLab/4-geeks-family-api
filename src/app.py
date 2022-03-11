14d6234cc0133a576584a73fe8716529013957be
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-11 20:11:36 +0000

    Add default members. Implement GET /members/<id> endpoint

--- src/app.py
+++ src/app.py
@@ -14,6 +14,10 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+# add default members
+jackson_family.add_member({ "name": "Jhon", "age": 33, "lucky_numbers": [ 7, 13, 22 ]})
+jackson_family.add_member({ "name": "Jane", "age": 35, "lucky_numbers": [ 10, 14, 3 ]})
+jackson_family.add_member({ "name": "Jimmy", "age": 5, "lucky_numbers": [ 1 ]})
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -27,16 +31,20 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def get_all_members():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
 
+    response_body = members
 
     return jsonify(response_body), 200
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+
+    if member == None:
+        return jsonify({ "message": "Member not found" }, 400)
+
+    return jsonify({ "member": member }, 200)
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
